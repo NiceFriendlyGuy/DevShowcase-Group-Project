efd52319423e597186bb8dabef9453b3cc70edd8
Ryen-K <ryen.kamkoum@gmail.com>
2025-05-13 19:22:18 +0200

    server.js: import db.config + routes mounting

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
+require('./config/db.config');
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const projectRoutes = require('./routes/projects.route');
+const accountRoutes = require('./routes/accounts.route');
+
 app.get('/', (req, res) => {
     res.send(`server running on port ${port}: Portfolio Web App`);
 })
+
+app.use(express.json());
+
+app.use('/api/projects', projectRoutes);
+app.use('/api/accounts', accountRoutes);
 
 app.listen(port, () => {
 console.log(`server running at http://localhost:${port}`);
