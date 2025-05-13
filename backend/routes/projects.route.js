b9b5750cd07991d173bd8b1f50eebac4468677d6
Ryen-K <ryen.kamkoum@gmail.com>
2025-05-13 04:43:14 +0200

    projects.route.js: done & to be tested

---
+++ backend/routes/projects.route.js
@@ -0,0 +1,10 @@
+const projectController = require('../controllers/projects.controller');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', projectController.findAll);
+// router.post('/projects', projectController.create);
+// router.put('/projects/:id', projectController.update);
+// router.delete('/projects/:id', projectController.delete);
+
+module.exports = router;
