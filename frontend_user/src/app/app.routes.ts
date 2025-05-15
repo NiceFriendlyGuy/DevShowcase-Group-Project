0d54f568325853fd708fd0b04e6050e60f1a768a
JoelSDS <joel.santos-dasilva@hotmail.com>
2025-05-15 17:26:59 +0200

    Affichage projets

--- frontend_user/src/app/app.routes.ts
+++ frontend_user/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'home', // Nouvelle route pour HomeComponent
+    component: HomeComponent,
   },
 ];
