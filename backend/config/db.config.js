d83f3bcb39ad793c8da8518561c2de14b6264d5a
Ryen-K <ryen.kamkoum@gmail.com>
2025-05-15 18:11:22 +0200

    install mongoose + minor adjustments

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/portfoliodb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect('mongodb://localhost:27017/portfoliodb')
 .then(() => console.log('connection to database: successful'))
 .catch((err) => console.error('Database error: ', err))
 
