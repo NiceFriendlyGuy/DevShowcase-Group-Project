56098165584eca1829c2e205bdcd84e185234db5
Antonio Gonzalez <antonio.gonzalez.puertas@gmail.com>
2025-05-15 19:00:04 +0200

    add projects

--- frontend_user/src/app/projects-tab/projects.page.ts
+++ frontend_user/src/app/projects-tab/projects.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
 @Component({
   selector: 'app-projects',
   templateUrl: 'projects.page.html',
   styleUrls: ['projects.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, HomeComponent],
 })
 export class ProjectsPage {
   constructor() {}
