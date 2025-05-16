8d2a729d672d93beac2858512e079e9c7ecccd80
JoelSDS <joel.santos-dasilva@hotmail.com>
2025-05-16 19:22:10 +0200

    ProjectCard version utilisable

--- frontend_user/src/app/components/project/project.component.ts
+++ frontend_user/src/app/components/project/project.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {  Component, inject, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonChip, IonIcon, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-project',
+  standalone: true,
+  imports: [CommonModule, IonChip, IonIcon, IonLabel,IonCard, IonCardHeader, IonCardTitle, IonCardContent],
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss'],
+
+
 })
 export class ProjectComponent  implements OnInit {
-
+  @Input() project:any;
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.project);
+  }
 
+  getIcon(technology: string, type:string): string {
+    let name = technology.toLowerCase().replace('.','');
+    return'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/' + name + '/' + name + '-' + type + '.svg';
+  }
+
+  getIconUrl(skillName: string): string {
+    const baseUrl = 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/';
+    const primaryUrl = `${baseUrl}${skillName.toLowerCase().replace('.','')}/${skillName.toLowerCase().replace('.','')}-original.svg`;
+    return primaryUrl;
+  }
 }
--- frontend_user/src/app/projects-tab/projects.page.ts
+++ frontend_user/src/app/projects-tab/projects.page.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
+import {ProjectComponent} from '../components/project/project.component';
+import { ProjectsService } from '../services/projects.service';
+import { ProfilesService } from '../services/profiles.service';
+
 @Component({
   selector: 'app-projects',
   templateUrl: 'projects.page.html',
   styleUrls: ['projects.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, HomeComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, HomeComponent, ProjectComponent],
 })
 export class ProjectsPage {
+  private projectsService: ProjectsService = inject<ProjectsService>(ProjectsService);
+  private profilesService: ProfilesService = inject<ProfilesService>(ProfilesService);
+  public projects: any[] = [];
+  
   constructor() {}
+
+  ngOnInit() {
+    this.projects= this.projectsService.getProjectsAll();
+    this.getPreviewAuthors();
+  }
+  
+  //CHANGER LE TYPE DE LA VARIABLE AUTHOR EN STRING PEUT ETRE ??
+  //Peut etre renvoyer direment le profile pour le getprofilebyid car il y aura tjrs que 1 profile retourner 
+  public getPreviewAuthors() {
+    this.projects.forEach((project) => {
+      const previewAuthors: { userId: string; name: string; surname: string }[] = [];
+      project.authors.forEach((authorId: number) => {
+        const profiles = this.profilesService.getProfilesById(authorId);
+        const profile = Array.isArray(profiles) ? profiles[0] : profiles;
+        if (profile) {
+          previewAuthors.push({
+            userId: profile.userId,
+            name: profile.name,
+            surname: profile.surname
+          });
+        }
+      });
+      project.authors = previewAuthors;
+    });
+  }
+    
 }
