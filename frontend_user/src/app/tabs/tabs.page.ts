98cb46f016c2c4544e06d95dcd70174c80b3ee4c
Antonio Gonzalez <antonio.gonzalez.puertas@gmail.com>
2025-05-17 19:54:36 +0200

    change accoount tab with logIn or account

--- frontend_user/src/app/tabs/tabs.page.ts
+++ frontend_user/src/app/tabs/tabs.page.ts
@@ -2,16 +2,17 @@ import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square, people, logIn, person, laptop } from 'ionicons/icons';
-
+import { AuthService } from '../services/auth.service'; 
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [CommonModule,IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
-
+  public authService = inject(AuthService);
   constructor() {
     addIcons({ triangle, ellipse, square, person, people, logIn, laptop });
   }
