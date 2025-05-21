d787256df39085b65d6138f420b5c25b54acac64
Ryen-K <ryen.kamkoum@gmail.com>
2025-05-21 23:37:33 +0200

    profiles.controller + route: GET -> POST /findAll

--- backend/controllers/projects.controller.js
+++ backend/controllers/projects.controller.js
@@ -1,14 +1,26 @@
-const Project = require('../models/projects.model');
+const Project = require('../models/project.model');
 const mongoose = require('mongoose');
 
 const projectController = {};
 
+/*
 projectController.findAll = async function (req, res) {
     try {
         const projects = await Project.find();
         res.status(200).json(projects);
     } catch(err) {
         res.status(500).json( { message: 'Server Error', error: err.message } );
+    }
+}
+*/
+
+projectController.findAll = async function (req, res) {
+    const filter = req.body || {};
+    try {
+        const projects = await Project.find(filter);
+        res.status(200).json(projects);
+    } catch(err) {
+        res.status(400).json({message: 'Failed to load projects', error:err.message});
     }
 }
 
--- backend/routes/projects.route.js
+++ backend/routes/projects.route.js
@@ -2,7 +2,9 @@ const projectController = require('../controllers/projects.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', projectController.findAll);
+// router.get('/', projectController.findAll);
+
+router.post('/findAll', projectController.findAll);
 router.post('/', projectController.create);
 router.put('/:id', projectController.update);
 router.delete('/:id', projectController.delete);
