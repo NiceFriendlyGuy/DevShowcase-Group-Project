09e8a30444d851099f31e3bb7e17027df4b99e8d
Gianni Contreras <Gianni.contreras96@hotmail.fr>
2025-05-22 20:35:36 +0200

    Adding Frontend_Admin Dashboard Folder

---
+++ frontend_admin/.angular/cache/19.2.13/dashboard_admin/vite/deps/@angular_material_divider.js
@@ -0,0 +1,17 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-VWNZL6SJ.js";
+import "./chunk-IBYU652R.js";
+import "./chunk-EDZ7BPPY.js";
+import "./chunk-OZPQS7KZ.js";
+import "./chunk-FMLYEA5T.js";
+import "./chunk-4VLR43KY.js";
+import "./chunk-ESHZKJMA.js";
+import "./chunk-GVWX775V.js";
+import "./chunk-HHP72VUM.js";
+import "./chunk-S35MAB2V.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
---
+++ frontend_admin/.angular/cache/19.2.13/dashboard_admin/vite/deps/chunk-N3TNKZGI.js
@@ -0,0 +1,41 @@
+import {
+  MatRipple
+} from "./chunk-HPCNK23V.js";
+import {
+  MatCommonModule
+} from "./chunk-EDZ7BPPY.js";
+import {
+  NgModule,
+  setClassMetadata,
+  ɵɵdefineInjector,
+  ɵɵdefineNgModule
+} from "./chunk-HHP72VUM.js";
+
+// node_modules/@angular/material/fesm2022/index-SYVYjXwK.mjs
+var MatRippleModule = class _MatRippleModule {
+  static ɵfac = function MatRippleModule_Factory(__ngFactoryType__) {
+    return new (__ngFactoryType__ || _MatRippleModule)();
+  };
+  static ɵmod = ɵɵdefineNgModule({
+    type: _MatRippleModule,
+    imports: [MatCommonModule, MatRipple],
+    exports: [MatRipple, MatCommonModule]
+  });
+  static ɵinj = ɵɵdefineInjector({
+    imports: [MatCommonModule, MatCommonModule]
+  });
+};
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(MatRippleModule, [{
+    type: NgModule,
+    args: [{
+      imports: [MatCommonModule, MatRipple],
+      exports: [MatRipple, MatCommonModule]
+    }]
+  }], null, null);
+})();
+
+export {
+  MatRippleModule
+};
+//# sourceMappingURL=chunk-N3TNKZGI.js.map
