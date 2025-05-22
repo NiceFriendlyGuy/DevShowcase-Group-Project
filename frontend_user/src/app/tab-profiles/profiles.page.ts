b90411dcd3dff35cfbcbe36085ebab921a25e9f6
Antonio Gonzalez <antonio.gonzalez.puertas@gmail.com>
2025-05-22 09:25:56 +0200

    add horizontal items filter

--- frontend_user/src/app/tab-profiles/profiles.page.ts
+++ frontend_user/src/app/tab-profiles/profiles.page.ts
@@ -4,13 +4,12 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
-  IonIcon,
-  IonItem,
 } from '@ionic/angular/standalone';
 import { ProfilesService } from 'src/app/services/profiles.service';
 import { ProfileComponent } from '../components/profile/profile.component';
 import { FormsModule } from '@angular/forms';
 import { SearchComponent } from '../components/utils/search/search.component';
+import { HorizontalFilterComponent } from '../components/utils/horizontal-filter/horizontal-filter.component';
 
 @Component({
   selector: 'app-profiles',
@@ -20,12 +19,11 @@ import { SearchComponent } from '../components/utils/search/search.component';
     ProfileComponent,
     IonHeader,
     IonToolbar,
-    IonItem,
     IonTitle,
     IonContent,
-    IonIcon,
     FormsModule,
     SearchComponent,
+    HorizontalFilterComponent,
   ],
 })
 export class ProfilesPage {
@@ -41,33 +39,26 @@ export class ProfilesPage {
   onFilteredProfiles(filtered: any[]) {
     this.filteredProfiles = filtered;
   }
+
+  onFilteredItems(filtered: any[]) {
+    this.selectedTechnologies = filtered;
+
+    console.log('Selected technologies:', this.selectedTechnologies);
+  }
   constructor() {}
   ngOnInit() {
     this.profiles = this.profilesService.getProfilesAll();
-    this.listTechnologies = this.profilesService
+
+    let technologiesNames: any[] = this.profilesService
       .getTechnologiesFromUsers()
       .sort((a: any, b: any) => a.name.localeCompare(b.name));
-    //this.updateFilteredProfiles();
-  }
 
-  ////////////////////////  Filters ///////////////////////////////////
-  toggleTech(tech: any) {
-    const index = this.selectedTechnologies.indexOf(tech);
-    if (index > -1) {
-      // Remove
-      this.selectedTechnologies = [
-        ...this.selectedTechnologies.slice(0, index),
-        ...this.selectedTechnologies.slice(index + 1),
-      ];
-    } else {
-      // Add
-      this.selectedTechnologies = [...this.selectedTechnologies, tech];
-    }
-    //this.updateFilteredProfiles();
-  }
-
-  public isSelected(tech: any): boolean {
-    return this.selectedTechnologies.includes(tech);
+    this.listTechnologies = technologiesNames.map((item: any) => {
+      return {
+        name: item.name,
+        src: this.getIconUrl(item.name),
+      };
+    });
   }
 
   getIconUrl(skillName: string): string {
