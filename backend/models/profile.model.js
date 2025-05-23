d4006a635ced6badef7d902a02950406fdb4971b
Ryen-K <ryen.kamkoum@gmail.com>
2025-05-23 20:59:37 +0200

    profile.model: update & adjust the schema according to API docs

--- backend/models/profile.model.js
+++ backend/models/profile.model.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose');
 
 const profileSchema = new mongoose.Schema ({
-    admin: Boolean, // was String in the API docs
+    admin: Boolean, 
     name: String,
     surname: String,
+    password: String,  
     role: String,
     bio: String,
-    email: String,  
-    password: String, // missing in the API docs                   
+    email: String,            
     phone: String,         
     photo: String,             
     technologies: [{
       name: String,
-      version: String
-    }]       
+      version: String,
+      _id: false
+    }],
+    isDeleted: Boolean
   }, {
     timestamps: true // adds createdAt & updatedAt automatically
-  })
+  });
 
 
 const Profile = mongoose.model("Profile", profileSchema);
