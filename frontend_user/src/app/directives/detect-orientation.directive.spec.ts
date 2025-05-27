41519a1e726a6c3a5e65149bd47d4906035faa91
JoelSDS <joel.santos-dasilva@hotmail.com>
2025-05-27 18:12:03 +0200

    Création Form Project

--- frontend_user/src/app/directives/detect-orientation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DetectOrientationDirective } from './detect-orientation.directive';
-
-describe('DetectOrientationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DetectOrientationDirective();
-    expect(directive).toBeTruthy();
-  });
-});
