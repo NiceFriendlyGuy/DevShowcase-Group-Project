81ad901dcd7cbccd3f6e7cc5a278abd122ba37c7
Antonio Gonzalez <antonio.gonzalez.puertas@gmail.com>
2025-06-05 16:42:17 +0200

    add Google Sigin

--- frontend_user/src/environments/environment.prod.ts
+++ frontend_user/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   BASE_URL_PROFILES: 'http://10.1.1.127:4000/api/profiles',
   hideContactDetails: true,
+  googleClientId: '',
 };
--- frontend_user/src/environments/environment.ts
+++ frontend_user/src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   BASE_URL_PROFILES: 'http://13.219.82.203:3333/api/profiles',
   BASE_URL: 'http://13.219.82.203:3333/api',
   hideContactDetails: true,
+  googleClientId: '',
 };
 
 /*
