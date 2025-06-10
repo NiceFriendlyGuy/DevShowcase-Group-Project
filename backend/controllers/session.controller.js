0c53ec13725e74fe74b50279f90e9fceee631335
Ryen-K <ryen.kamkoum@gmail.com>
2025-06-10 19:18:01 +0200

    add console log for test in session.controller

--- backend/controllers/session.controller.js
+++ backend/controllers/session.controller.js
@@ -6,6 +6,7 @@ const sessionController ={};
 sessionController.findAll = async function (req, res) {
     console.log ("Params", req.params);
     const filter = req?.params?.email?{"email":req.params?.email} :{};
+    console.log('filter= ', filter);
     try {
         const sessions = await Session.find(filter);
         res.status(200).json(sessions);
