3316d4cad9a3f1d7999af6ec77b3fd1e065de09c
Ryen-K <ryen.kamkoum@gmail.com>
2025-06-10 19:10:12 +0200

    Add docker-compose + Dockerfile to git

--- backend/routes/session.route.js
+++ backend/routes/session.route.js
@@ -2,6 +2,6 @@ const sessionController = require('../controllers/session.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', sessionController.findAll);
+router.get('/:email', sessionController.findAll);
 
 module.exports = router;
