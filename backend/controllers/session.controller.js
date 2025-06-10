cc9f33a98ed50d161c9c610b6af3de76d4bf4cd8
Ryen-K <ryen.kamkoum@gmail.com>
2025-06-10 20:03:59 +0200

    New routes added: findOnLine, findOffLine

--- backend/controllers/session.controller.js
+++ backend/controllers/session.controller.js
@@ -3,9 +3,33 @@ const mongoose = require('mongoose');
 
 const sessionController ={};
 
-sessionController.findAll = async function (req, res) {
+sessionController.findByEmail = async function (req, res) {
     console.log ("body", req.body);
     const filter = req?.body?.email?{"email":req.body?.email} :{};
+    console.log('filter= ', filter);
+    try {
+        const sessions = await Session.find(filter);
+        res.status(200).json(sessions);
+    } catch(err) {
+        res.status(400).json({message: 'Failed to load sessions', error:err.message});
+    }
+}
+
+sessionController.findOnline = async function (req, res) {
+    console.log ("body", req.body);
+    const filter = {"status":0};
+    console.log('filter= ', filter);
+    try {
+        const sessions = await Session.find(filter);
+        res.status(200).json(sessions);
+    } catch(err) {
+        res.status(400).json({message: 'Failed to load sessions', error:err.message});
+    }
+}
+
+sessionController.findOffline = async function (req, res) {
+    console.log ("body", req.body);
+    const filter = {"status":1};
     console.log('filter= ', filter);
     try {
         const sessions = await Session.find(filter);
